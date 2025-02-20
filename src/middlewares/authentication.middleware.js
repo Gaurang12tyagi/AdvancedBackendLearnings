92f31da8c49f0cbe94615fdb7a1c05318483cf4c
Gaurang12tyagi <126134404+Gaurang12tyagi@users.noreply.github.com>
2025-02-21 01:46:58 +0530

    modelling for tweets ,playlist,like,comments

--- src/middlewares/authentication.middleware.js
+++ src/middlewares/authentication.middleware.js
@@ -2,6 +2,8 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiErrors.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from 'jsonwebtoken'
+
+
 export const verifyJWT = asyncHandler(async (req, res, next) => {
 
   try {
